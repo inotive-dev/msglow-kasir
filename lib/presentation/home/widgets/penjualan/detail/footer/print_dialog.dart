612ac85c6ba99d75c697e0c997b4c88808a7d746
myunusss <turmudi.yunus@gmail.com>
2022-09-01 11:44:19 +0700

    update version

--- lib/presentation/home/widgets/penjualan/detail/footer/print_dialog.dart
+++ lib/presentation/home/widgets/penjualan/detail/footer/print_dialog.dart
@@ -72,36 +72,32 @@ class PrintDialog extends StatelessWidget {
             child: BlocBuilder<PrinterCubit, PrinterState>(
               buildWhen: (previous, current) => previous.isPrinterConnected != current.isPrinterConnected,
               builder: (context, state) {
-                // TODO: Delete soon
-                return PrintConfirmation(
-                  args: args,
-                );
-                // if (_kDisablePrinterDetection) {
-                //   return PrintConfirmation(
-                //     args: args,
-                //   );
-                // }
+                if (_kDisablePrinterDetection) {
+                  return PrintConfirmation(
+                    args: args,
+                  );
+                }
 
-                // return state.isPrinterConnected.maybeWhen(
-                //   success: (isPrinterConnected) {
-                //     if (isPrinterConnected) {
-                //       return PrintConfirmation(
-                //         args: args,
-                //       );
-                //     }
-                //     return SingleChildScrollView(
-                //       padding: EdgeInsets.symmetric(
-                //         vertical: Sizes.height16,
-                //       ),
-                //       child: PrintContent(
-                //         args: args,
-                //       ),
-                //     );
-                //   },
-                //   orElse: () {
-                //     return const CircularProgressIndicator();
-                //   },
-                // );
+                return state.isPrinterConnected.maybeWhen(
+                  success: (isPrinterConnected) {
+                    if (isPrinterConnected) {
+                      return PrintConfirmation(
+                        args: args,
+                      );
+                    }
+                    return SingleChildScrollView(
+                      padding: EdgeInsets.symmetric(
+                        vertical: Sizes.height16,
+                      ),
+                      child: PrintContent(
+                        args: args,
+                      ),
+                    );
+                  },
+                  orElse: () {
+                    return const CircularProgressIndicator();
+                  },
+                );
               },
             ),
           ),
